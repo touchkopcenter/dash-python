6b91d4a500dfd7c38a3a27c1622f17704d0fb910
touchkopcenter <touchkopcenter@gmail.com>
2024-02-05 14:28:45 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Dash Default Template: mydashpy
+
+Created on 2024-02-05 13:00:28.097943
+
+Welcome to your [Plotly Dash](https://plotly.com/dash/) App! This is a template for your mydashpy app.
+
+See the [Dash Documentation](https://dash.plotly.com/introduction) for more information on how to get your app up and running.
+
+## Running the App
+
+Run `src/app.py` and navigate to http://127.0.0.1:8050/ in your browser.
---
+++ src/app.py
@@ -0,0 +1,39 @@
+
+'''
+ # @ Create Time: 2024-02-05 13:00:28.101631
+'''
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__, title="mydashpy")
+
+# Declare server for Heroku deployment. Needed for Procfile.
+server = app.server
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
